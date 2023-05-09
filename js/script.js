01131bb405d734e34c09a4840c2e97923e5523a6
Re-games-web <117376944+Re-games-web@users.noreply.github.com>
2023-05-09 21:21:00 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,1 @@
-$(function() {
 
-    $('#myForm').on('submit', function(event) {
-        event.preventDefault();
-        var iId = $('#iId').val();
-        $.ajax({
-            type: 'POST',
-            url: 'index.php',
-            data: { iId: iId },
-            success: function(response) {
-                $('#result').html(response);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
-    });
-});
